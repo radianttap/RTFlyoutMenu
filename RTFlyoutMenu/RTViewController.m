4fe9eadd3db7a539cddaaaf45b6da72b956b935a
Aleksandar Vacic <av@radianttap.com>
2012-11-27 11:46:53 +0100

    Initial Commit

---
+++ RTFlyoutMenu/RTViewController.m
@@ -0,0 +1,29 @@
+//
+//  RTViewController.m
+//  RTFlyoutMenu
+//
+//  Created by Aleksandar Vacić on 27.11.12..
+//  Copyright (c) 2012. Aleksandar Vacić. All rights reserved.
+//
+
+#import "RTViewController.h"
+
+@interface RTViewController ()
+
+@end
+
+@implementation RTViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
